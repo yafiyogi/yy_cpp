ea97b2e9489a52f9eaeddf9e1fae5cfcd02239a0
yafiyogi <yafiyogi@users.noreply.github.com>
2020-05-28 11:34:28 +0100

    Change copyright notice.

--- yy_func_traits.h
+++ yy_func_traits.h
@@ -1,6 +1,8 @@
+/*
+
 MIT License
 
-Copyright (c) 2020 Yafiyogi
+Copyright (c) 2019 Yafiyogi
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -19,3 +21,43 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+
+*/
+
+#ifndef yy_func_traits_h
+# define yy_func_traits_h
+
+namespace yafiyogi {
+
+template<typename T>
+struct yy_func_traits:
+            public yy_func_traits<decltype(&T::operator())>
+{
+};
+
+template<typename C, typename R, typename... Args>
+struct yy_func_traits<R(C::*)(Args...) const>
+{
+    using result_type = R;
+
+    template<size_t N>
+    struct arg
+    {
+        using type = typename std::tuple_element<N, std::tuple<Args...>>::type;
+    };
+};
+
+template<typename C, typename... Args>
+struct yy_func_traits<void(C::*)(Args...) const>
+{
+    using result_type = void;
+
+    template<size_t N>
+    struct arg
+    {
+        using type = typename std::tuple_element<N, std::tuple<Args...>>::type;
+    };
+};
+
+} // namespace yafiyogi
+#endif // yy_func_traits_h
