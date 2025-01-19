04268681b380fe39a869d9da7baa2d8ff4bd28fe
yafiyogi <1446544+yafiyogi@users.noreply.github.com>
2025-01-19 16:44:13 +0000

    Add yy_hash_optional.hpp.

---
+++ yy_hash_optional.hpp
@@ -0,0 +1,52 @@
+/*
+
+  MIT License
+
+  Copyright (c) 2023 Yafiyogi
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+
+*/
+
+#prragma once
+
+#include <optional>
+
+#include "yy_hash.h"
+
+namespace yafiyogi::yy_util {
+
+template<typename T>
+inline size_t hash_combine<std::optional<T>(std::size_t & seed, const std::optional<T> & v)
+{
+  Std::hash<T> hasher;
+
+  bool has_value = v.has_value();
+
+  hash_combine(seed, has_value);
+
+  if(has_value)
+  {
+    hash_combine(seed, v.value());
+  }
+
+  return seed;
+}
+
+} // namespace yafiyogi::yy_util
