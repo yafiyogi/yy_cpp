c0451cc4f533504ae0a03b477932f382a07511ff
yafiyogi <1446544+yafiyogi@users.noreply.github.com>
2025-01-24 16:29:31 +0000

    Add 'span<>', 'const_span<>' & 'static_flat_map<>' tests.

---
+++ unit_tests/span_tests.cpp
@@ -0,0 +1,311 @@
+/*
+
+  MIT License
+
+  Copyright (c) 2024 Yafiyogi
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+
+*/
+
+#include <string>
+#include <string_view>
+
+#include "gtest/gtest.h"
+
+#include "yy_span.h"
+
+namespace yafiyogi::yy_cpp::tests {
+
+class TestSpan:
+      public testing::Test
+{
+  public:
+    void SetUp() override
+    {
+    }
+
+    void TearDown() override
+    {
+    }
+};
+
+TEST_F(TestSpan, span_constructors)
+{
+  std::string str = "str";
+
+  auto span = yafiyogi::yy_quad::make_span(str);
+
+  EXPECT_EQ(str, span.data());
+
+  EXPECT_EQ(str, span);
+}
+
+TEST_F(TestSpan, span_inc_begin)
+{
+  std::string str = "str";
+
+  EXPECT_EQ(str.substr(1), yafiyogi::yy_quad::make_span(str).inc_begin());
+  EXPECT_EQ(str.substr(2), yafiyogi::yy_quad::make_span(str).inc_begin(2));
+  EXPECT_EQ(str.substr(3), yafiyogi::yy_quad::make_span(str).inc_begin(3));
+  EXPECT_EQ(std::string_view{}, yafiyogi::yy_quad::make_span(str).inc_begin(3));
+  EXPECT_EQ(std::string_view{}, yafiyogi::yy_quad::make_span(str).inc_begin(4));
+}
+
+TEST_F(TestSpan, span_dec_end)
+{
+  std::string str = "str";
+
+  EXPECT_EQ(str.substr(0, str.size() - 1), yafiyogi::yy_quad::make_span(str).dec_end());
+  EXPECT_EQ(str.substr(0, str.size() - 2), yafiyogi::yy_quad::make_span(str).dec_end(2));
+  EXPECT_EQ(str.substr(0, str.size() - 3), yafiyogi::yy_quad::make_span(str).dec_end(3));
+  EXPECT_EQ(std::string_view{}, yafiyogi::yy_quad::make_span(str).dec_end(3));
+  EXPECT_EQ(std::string_view{}, yafiyogi::yy_quad::make_span(str).dec_end(4));
+}
+
+TEST_F(TestSpan, span_subspan)
+{
+  std::string str = "1234567890";
+
+  EXPECT_EQ(str.substr(1), yafiyogi::yy_quad::make_span(str).subspan(1));
+  EXPECT_EQ(str.substr(1, 3), yafiyogi::yy_quad::make_span(str).subspan(1, 3));
+  EXPECT_EQ(str.substr(1, 11), yafiyogi::yy_quad::make_span(str).subspan(1, 11));
+  EXPECT_EQ(str.substr(10, 2), yafiyogi::yy_quad::make_span(str).subspan(10, 2));
+  EXPECT_EQ(std::string_view{}, yafiyogi::yy_quad::make_span(str).subspan(10, 2));
+}
+
+TEST_F(TestSpan, span_find_pos)
+{
+  std::string str = "1234567890";
+
+  EXPECT_EQ(4, yafiyogi::yy_quad::make_span(str).find_pos('5'));
+  EXPECT_EQ(yafiyogi::yy_quad::span<char>::npos, yafiyogi::yy_quad::make_span(str).find_pos('A'));
+}
+
+TEST_F(TestSpan, span_lt)
+{
+  auto str_a = std::string{"A"};
+  auto str_b = std::string{"B"};
+  auto str_b2 = std::string{"B"};
+  auto str_c = std::string{"C"};
+
+  auto span_a = yafiyogi::yy_quad::make_span(str_a);
+  auto span_b = yafiyogi::yy_quad::make_span(str_b);
+  auto span_b2 = yafiyogi::yy_quad::make_span(str_b2);
+  auto span_c = yafiyogi::yy_quad::make_span(str_c);
+
+  EXPECT_TRUE(span_a < span_b);
+  EXPECT_FALSE(span_b < span_b2);
+  EXPECT_FALSE(span_c < span_b);
+
+  EXPECT_FALSE(span_b < str_a);
+  EXPECT_FALSE(span_b < str_b2);
+  EXPECT_TRUE(span_b < str_c);
+
+  EXPECT_TRUE(str_a < span_b);
+  EXPECT_FALSE(str_b2 < span_b);
+  EXPECT_FALSE(str_c < span_b);
+}
+
+TEST_F(TestSpan, span_eq)
+{
+  auto str_a = std::string{"A"};
+  auto str_b = std::string{"B"};
+  auto str_b2 = std::string{"B"};
+  auto str_c = std::string{"C"};
+
+  auto span_a = yafiyogi::yy_quad::make_span(str_a);
+  auto span_b = yafiyogi::yy_quad::make_span(str_b);
+  auto span_b2 = yafiyogi::yy_quad::make_span(str_b2);
+  auto span_c = yafiyogi::yy_quad::make_span(str_c);
+
+  EXPECT_FALSE(span_a == span_b);
+  EXPECT_TRUE(span_b == span_b2);
+  EXPECT_FALSE(span_c == span_b);
+
+  EXPECT_FALSE(span_b == str_a);
+  EXPECT_TRUE(span_b == str_b2);
+  EXPECT_FALSE(span_b == str_c);
+
+  EXPECT_FALSE(str_a == span_b);
+  EXPECT_TRUE(str_b2 == span_b);
+  EXPECT_FALSE(str_c == span_b);
+}
+
+TEST_F(TestSpan, span_empty)
+{
+  std::string str = "";
+
+  EXPECT_TRUE(yafiyogi::yy_quad::make_span(str).empty());
+  str = "str";
+  EXPECT_FALSE(yafiyogi::yy_quad::make_span(str).empty());
+}
+
+TEST_F(TestSpan, const_span_constructors)
+{
+  const char * str_1 = "str 1";
+  std::string_view str_2 = "str 2";
+  std::string str_3 = "str 2";
+
+  auto span_1 = yafiyogi::yy_quad::make_const_span(str_1);
+  auto span_2 = yafiyogi::yy_quad::make_const_span(str_2);
+  auto span_3 = yafiyogi::yy_quad::make_const_span(str_3);
+
+  EXPECT_EQ(str_1, span_1.data());
+  EXPECT_EQ(str_2, span_2.data());
+  EXPECT_EQ(str_3, span_3.data());
+
+  EXPECT_EQ(std::string_view{str_1}, span_1.data());
+  EXPECT_EQ(str_2, span_2);
+  EXPECT_EQ(str_3, span_3);
+}
+
+TEST_F(TestSpan, const_span_inc_begin)
+{
+  std::string_view str = "str";
+
+  EXPECT_EQ(str.substr(1), yafiyogi::yy_quad::make_const_span(str).inc_begin());
+  EXPECT_EQ(str.substr(2), yafiyogi::yy_quad::make_const_span(str).inc_begin(2));
+  EXPECT_EQ(str.substr(3), yafiyogi::yy_quad::make_const_span(str).inc_begin(3));
+  EXPECT_EQ(std::string_view{}, yafiyogi::yy_quad::make_const_span(str).inc_begin(3));
+  EXPECT_EQ(std::string_view{}, yafiyogi::yy_quad::make_const_span(str).inc_begin(4));
+}
+
+TEST_F(TestSpan, const_span_dec_end)
+{
+  std::string_view str = "str";
+
+  EXPECT_EQ(str.substr(0, str.size() - 1), yafiyogi::yy_quad::make_const_span(str).dec_end());
+  EXPECT_EQ(str.substr(0, str.size() - 2), yafiyogi::yy_quad::make_const_span(str).dec_end(2));
+  EXPECT_EQ(str.substr(0, str.size() - 3), yafiyogi::yy_quad::make_const_span(str).dec_end(3));
+  EXPECT_EQ(std::string_view{}, yafiyogi::yy_quad::make_const_span(str).dec_end(3));
+  EXPECT_EQ(std::string_view{}, yafiyogi::yy_quad::make_const_span(str).dec_end(4));
+}
+
+TEST_F(TestSpan, const_span_subspan)
+{
+  std::string_view str = "1234567890";
+
+  EXPECT_EQ(str.substr(1), yafiyogi::yy_quad::make_const_span(str).subspan(1));
+  EXPECT_EQ(str.substr(1, 3), yafiyogi::yy_quad::make_const_span(str).subspan(1, 3));
+  EXPECT_EQ(str.substr(1, 11), yafiyogi::yy_quad::make_const_span(str).subspan(1, 11));
+  EXPECT_EQ(str.substr(10, 2), yafiyogi::yy_quad::make_const_span(str).subspan(10, 2));
+  EXPECT_EQ(std::string_view{}, yafiyogi::yy_quad::make_const_span(str).subspan(10, 2));
+}
+
+TEST_F(TestSpan, const_span_find_pos)
+{
+  std::string str = "1234567890";
+
+  EXPECT_EQ(4, yafiyogi::yy_quad::make_const_span(str).find_pos('5'));
+  EXPECT_EQ(yafiyogi::yy_quad::const_span<char>::npos, yafiyogi::yy_quad::make_const_span(str).find_pos('A'));
+}
+
+TEST_F(TestSpan, const_span_lt)
+{
+  auto str_a = std::string{"A"};
+  auto str_b = std::string{"B"};
+  auto str_b2 = std::string{"B"};
+  auto str_c = std::string{"C"};
+
+  auto const_span_a = yafiyogi::yy_quad::make_const_span(str_a);
+  auto const_span_b = yafiyogi::yy_quad::make_const_span(str_b);
+  auto const_span_b2 = yafiyogi::yy_quad::make_const_span(str_b2);
+  auto const_span_c = yafiyogi::yy_quad::make_const_span(str_c);
+
+  EXPECT_TRUE(const_span_a < const_span_b);
+  EXPECT_FALSE(const_span_b < const_span_b2);
+  EXPECT_FALSE(const_span_c < const_span_b);
+
+  EXPECT_FALSE(const_span_b < str_a);
+  EXPECT_FALSE(const_span_b < str_b2);
+  EXPECT_TRUE(const_span_b < str_c);
+
+  EXPECT_TRUE(str_a < const_span_b);
+  EXPECT_FALSE(str_b2 < const_span_b);
+  EXPECT_FALSE(str_c < const_span_b);
+
+  auto str_a_2 = std::string{"A"};
+  auto str_b_2 = std::string{"B"};
+  auto str_c_2 = std::string{"C"};
+
+  auto span_a = yafiyogi::yy_quad::make_span(str_a_2);
+  auto span_b = yafiyogi::yy_quad::make_span(str_b_2);
+  auto span_c = yafiyogi::yy_quad::make_span(str_c_2);
+
+  EXPECT_FALSE(const_span_b < span_a);
+  EXPECT_FALSE(const_span_b < span_b);
+  EXPECT_TRUE(const_span_b < span_c);
+
+  EXPECT_TRUE(span_a < const_span_b);
+  EXPECT_FALSE(span_b < const_span_b);
+  EXPECT_FALSE(span_c < const_span_b);
+}
+
+TEST_F(TestSpan, const_span_eq)
+{
+  auto str_a = std::string{"A"};
+  auto str_b = std::string{"B"};
+  auto str_b2 = std::string{"B"};
+  auto str_c = std::string{"C"};
+
+  auto const_span_a = yafiyogi::yy_quad::make_const_span(str_a);
+  auto const_span_b = yafiyogi::yy_quad::make_const_span(str_b);
+  auto const_span_b2 = yafiyogi::yy_quad::make_const_span(str_b2);
+  auto const_span_c = yafiyogi::yy_quad::make_const_span(str_c);
+
+  EXPECT_FALSE(const_span_a == const_span_b);
+  EXPECT_TRUE(const_span_b == const_span_b2);
+  EXPECT_FALSE(const_span_c == const_span_b);
+
+  EXPECT_FALSE(const_span_b == str_a);
+  EXPECT_TRUE(const_span_b == str_b2);
+  EXPECT_FALSE(const_span_b == str_c);
+
+  EXPECT_FALSE(str_a == const_span_b);
+  EXPECT_TRUE(str_b2 == const_span_b);
+  EXPECT_FALSE(str_c == const_span_b);
+
+  auto str_a_2 = std::string{"A"};
+  auto str_b_2 = std::string{"B"};
+  auto str_c_2 = std::string{"C"};
+
+  auto span_a = yafiyogi::yy_quad::make_span(str_a_2);
+  auto span_b = yafiyogi::yy_quad::make_span(str_b_2);
+  auto span_c = yafiyogi::yy_quad::make_span(str_c_2);
+
+  EXPECT_FALSE(const_span_b == span_a);
+  EXPECT_TRUE(const_span_b == span_b);
+  EXPECT_FALSE(const_span_b == span_c);
+
+  EXPECT_FALSE(span_a == const_span_b);
+  EXPECT_TRUE(span_b == const_span_b);
+  EXPECT_FALSE(span_c == const_span_b);
+}
+
+TEST_F(TestSpan, const_span_empty)
+{
+  std::string str = "";
+
+  EXPECT_TRUE(yafiyogi::yy_quad::make_const_span(str).empty());
+  str = "str";
+  EXPECT_FALSE(yafiyogi::yy_quad::make_const_span(str).empty());
+}
+
+} // namespace yafiyogi::yy_cpp::tests
---
+++ unit_tests/static_flat_map_tests.cpp
@@ -0,0 +1,474 @@
+/*
+
+  MIT License
+
+  Copyright (c) 2024 Yafiyogi
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+
+*/
+
+#include "gtest/gtest.h"
+
+#include "yy_static_flat_map.h"
+
+namespace yafiyogi::yy_cpp::tests {
+
+class TestStaticFlatMap:
+      public testing::Test
+{
+  public:
+    void SetUp() override
+    {
+    }
+
+    void TearDown() override
+    {
+    }
+    using flat_map = yy_data::static_flat_map<int, int, 10>;
+
+};
+
+
+TEST_F(TestStaticFlatMap, LessThanSimple)
+{
+  {
+    flat_map vec_1{};
+    flat_map vec_2{};
+
+    EXPECT_FALSE(vec_1 < vec_2);
+  }
+
+  {
+    flat_map vec_1{};
+    flat_map vec_2{};
+
+    vec_2.emplace(1, 1);
+
+    EXPECT_TRUE(vec_1 < vec_2);
+  }
+
+  // vec_1 == vec_2
+  {
+    flat_map vec_1{};
+    flat_map vec_2{};
+
+    vec_1.emplace(1, 1);
+
+    vec_2.emplace(1, 1);
+
+    EXPECT_FALSE(vec_1 < vec_2);
+  }
+
+  // vec_1 < vec_2
+  {
+    flat_map vec_1{};
+    flat_map vec_2{};
+
+    vec_1.emplace(1, 1);
+
+    vec_2.emplace(1, 2);
+
+    EXPECT_TRUE(vec_1 < vec_2);
+  }
+
+  // vec_1 > vec_2
+  {
+    flat_map vec_1{};
+    flat_map vec_2{};
+
+    vec_1.emplace(1, 2);
+
+    vec_2.emplace(1, 1);
+
+    EXPECT_FALSE(vec_1 < vec_2);
+  }
+
+  // vec_1 key < vec_2 key
+  {
+    flat_map vec_1{};
+    flat_map vec_2{};
+
+    vec_1.emplace(1, 1);
+
+    vec_2.emplace(2, 2);
+
+    EXPECT_TRUE(vec_1 < vec_2);
+  }
+}
+
+TEST_F(TestStaticFlatMap, EqualToMultiple)
+{
+  {
+    flat_map vec_1{};
+    flat_map vec_2{};
+
+    vec_1.emplace(1, 1);
+
+    vec_2.emplace(1, 1);
+    vec_2.emplace(2, 2);
+
+    EXPECT_FALSE(vec_1 == vec_2);
+  }
+
+  // vec_1 == vec_2
+  {
+    flat_map vec_1{};
+    flat_map vec_2{};
+
+    vec_1.emplace(1, 1);
+    vec_1.emplace(2, 2);
+
+    vec_2.emplace(1, 1);
+    vec_2.emplace(2, 2);
+
+    EXPECT_TRUE(vec_1 == vec_2);
+  }
+
+  // vec_1 < vec_2
+  {
+    flat_map vec_1{};
+    flat_map vec_2{};
+
+    vec_1.emplace(1, 1);
+    vec_1.emplace(2, 2);
+
+    vec_2.emplace(1, 1);
+    vec_2.emplace(2, 3);
+
+    EXPECT_FALSE(vec_1 == vec_2);
+  }
+
+  // vec_1 > vec_2
+  {
+    flat_map vec_1{};
+    flat_map vec_2{};
+
+    vec_1.emplace(1, 1);
+    vec_1.emplace(2, 3);
+
+    vec_2.emplace(1, 1);
+    vec_2.emplace(2, 2);
+
+    EXPECT_FALSE(vec_1 == vec_2);
+  }
+
+  // vec_1 key < vec_2 key
+  {
+    flat_map vec_1{};
+    flat_map vec_2{};
+
+    vec_1.emplace(1, 1);
+    vec_1.emplace(2, 3);
+
+    vec_2.emplace(1, 1);
+    vec_2.emplace(3, 3);
+
+    EXPECT_FALSE(vec_1 == vec_2);
+  }
+}
+
+TEST_F(TestStaticFlatMap, EqualToSimple)
+{
+  {
+    flat_map vec_1{};
+    flat_map vec_2{};
+
+    EXPECT_TRUE(vec_1 == vec_2);
+  }
+
+  {
+    flat_map vec_1{};
+    flat_map vec_2{};
+
+    vec_2.emplace(1, 1);
+
+    EXPECT_FALSE(vec_1 == vec_2);
+  }
+
+  // vec_1 == vec_2
+  {
+    flat_map vec_1{};
+    flat_map vec_2{};
+
+    vec_1.emplace(1, 1);
+
+    vec_2.emplace(1, 1);
+
+    EXPECT_TRUE(vec_1 == vec_2);
+  }
+
+  // vec_1 < vec_2
+  {
+    flat_map vec_1{};
+    flat_map vec_2{};
+
+    vec_1.emplace(1, 1);
+
+    vec_2.emplace(1, 2);
+
+    EXPECT_FALSE(vec_1 == vec_2);
+  }
+
+  // vec_1 > vec_2
+  {
+    flat_map vec_1{};
+    flat_map vec_2{};
+
+    vec_1.emplace(1, 2);
+
+    vec_2.emplace(1, 1);
+
+    EXPECT_FALSE(vec_1 == vec_2);
+  }
+
+  // vec_1 key < vec_2 key
+  {
+    flat_map vec_1{};
+    flat_map vec_2{};
+
+    vec_1.emplace(1, 1);
+
+    vec_2.emplace(2, 2);
+
+    EXPECT_FALSE(vec_1 == vec_2);
+  }
+}
+
+TEST_F(TestStaticFlatMap, LessThanMultiple)
+{
+  {
+    flat_map vec_1{};
+    flat_map vec_2{};
+
+    EXPECT_FALSE(vec_1 < vec_2);
+  }
+
+  {
+    flat_map vec_1{};
+    flat_map vec_2{};
+
+    vec_1.emplace(1, 1);
+
+    vec_2.emplace(1, 1);
+    vec_2.emplace(2, 2);
+
+    EXPECT_TRUE(vec_1 < vec_2);
+  }
+
+  // vec_1 == vec_2
+  {
+    flat_map vec_1{};
+    flat_map vec_2{};
+
+    vec_1.emplace(1, 1);
+    vec_1.emplace(2, 2);
+
+    vec_2.emplace(1, 1);
+    vec_2.emplace(2, 2);
+
+    EXPECT_FALSE(vec_1 < vec_2);
+  }
+
+  // vec_1 < vec_2
+  {
+    flat_map vec_1{};
+    flat_map vec_2{};
+
+    vec_1.emplace(1, 1);
+    vec_1.emplace(2, 2);
+
+    vec_2.emplace(1, 1);
+    vec_2.emplace(2, 3);
+
+    EXPECT_TRUE(vec_1 < vec_2);
+  }
+
+  // vec_1 > vec_2
+  {
+    flat_map vec_1{};
+    flat_map vec_2{};
+
+    vec_1.emplace(1, 1);
+    vec_1.emplace(2, 3);
+
+    vec_2.emplace(1, 1);
+    vec_2.emplace(2, 2);
+
+    EXPECT_FALSE(vec_1 < vec_2);
+  }
+
+  // vec_1 key < vec_2 key
+  {
+    flat_map vec_1{};
+    flat_map vec_2{};
+
+    vec_1.emplace(1, 1);
+    vec_1.emplace(2, 3);
+
+    vec_2.emplace(1, 1);
+    vec_2.emplace(3, 3);
+
+    EXPECT_TRUE(vec_1 < vec_2);
+  }
+}
+
+TEST_F(TestStaticFlatMap, find)
+{
+  flat_map vec{};
+
+  vec.emplace(1, 1);
+  vec.emplace(2, 2);
+  vec.emplace(3, 3);
+
+  {
+    auto [key, value, pos] = vec.find(2);
+    ASSERT_NE(nullptr, key);
+    EXPECT_EQ(2, *key);
+    ASSERT_NE(nullptr, value);
+    EXPECT_EQ(2, *value);
+    EXPECT_EQ(1, pos);
+  }
+
+  {
+    auto [key, value, pos] = vec.find(0);
+    ASSERT_EQ(nullptr, key);
+    ASSERT_EQ(nullptr, value);
+    EXPECT_EQ(0, pos);
+  }
+
+  {
+    auto [key, value, pos] = vec.find(4);
+    ASSERT_EQ(nullptr, key);
+    ASSERT_EQ(nullptr, value);
+    EXPECT_EQ(3, pos);
+  }
+}
+
+TEST_F(TestStaticFlatMap, const_find)
+{
+  flat_map vec{};
+  const flat_map & const_vec = vec;
+
+  vec.emplace(1, 1);
+  vec.emplace(2, 2);
+  vec.emplace(3, 3);
+
+  {
+    auto [key, value, pos] = const_vec.find(2);
+    ASSERT_NE(nullptr, key);
+    EXPECT_EQ(2, *key);
+    ASSERT_NE(nullptr, value);
+    EXPECT_EQ(2, *value);
+    EXPECT_EQ(1, pos);
+  }
+
+  {
+    auto [key, value, pos] = const_vec.find(0);
+    ASSERT_EQ(nullptr, key);
+    ASSERT_EQ(nullptr, value);
+    EXPECT_EQ(0, pos);
+  }
+
+  {
+    auto [key, value, pos] = const_vec.find(10);
+    ASSERT_EQ(nullptr, key);
+    ASSERT_EQ(nullptr, value);
+    EXPECT_EQ(3, pos);
+  }
+}
+
+TEST_F(TestStaticFlatMap, find_value)
+{
+  flat_map vec{};
+
+  vec.emplace(1, 1);
+  vec.emplace(2, 2);
+  vec.emplace(3, 3);
+
+  {
+    auto [found_pos, found] = vec.find_value([](auto * value, auto pos) {
+      ASSERT_NE(nullptr, value);
+      EXPECT_EQ(2, *value);
+      EXPECT_EQ(1, pos);
+    }, 2);
+
+    EXPECT_EQ(1, found_pos);
+    EXPECT_TRUE(found);
+  }
+
+  {
+    auto [found_pos, found] = vec.find_value([](auto * /* value */, auto /* pos */) {
+      EXPECT_FALSE(true);
+    }, 0);
+
+    EXPECT_EQ(0, found_pos);
+    EXPECT_FALSE(found);
+  }
+
+  {
+    auto [found_pos, found] = vec.find_value([](auto * /* value */, auto /* pos */) {
+      EXPECT_FALSE(true);
+    }, 10);
+
+    EXPECT_EQ(3, found_pos);
+    EXPECT_FALSE(found);
+  }
+}
+
+TEST_F(TestStaticFlatMap, const_find_value)
+{
+  flat_map vec{};
+  const flat_map & const_vec = vec;
+
+  vec.emplace(1, 1);
+  vec.emplace(2, 2);
+  vec.emplace(3, 3);
+
+  {
+    auto [found_pos, found] = const_vec.find_value([](auto * value, auto pos) {
+      ASSERT_NE(nullptr, value);
+      EXPECT_EQ(2, *value);
+      EXPECT_EQ(1, pos);
+    }, 2);
+
+    EXPECT_EQ(1, found_pos);
+    EXPECT_TRUE(found);
+  }
+
+  {
+    auto [found_pos, found] = const_vec.find_value([](auto * /* value */, auto /* pos */) {
+      EXPECT_FALSE(true);
+    }, 0);
+
+    EXPECT_EQ(0, found_pos);
+    EXPECT_FALSE(found);
+  }
+
+  {
+    auto [found_pos, found] = const_vec.find_value([](auto * /* value */, auto /* pos */) {
+      EXPECT_FALSE(true);
+    }, 10);
+
+    EXPECT_EQ(3, found_pos);
+    EXPECT_FALSE(found);
+  }
+}
+
+} // namespace yafiyogi::yy_cpp::tests
