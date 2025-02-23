3a2ecde18ab9bd09f91c086d21ddb86f8ccb9e0f
yafiyogi <1446544+yafiyogi@users.noreply.github.com>
2025-02-23 03:11:14 +0000

    Update observer_ptr<>.

--- yy_observer_ptr.hpp
+++ yy_observer_ptr.hpp
@@ -1,16 +1,47 @@
-// Based on https://github.com/CppCon/CppCon2019/blob/master/Presentations/the_smart_pointers_i_wish_i_had/the_smart_pointers_i_wish_i_had__matthew_fleming__cppcon_2019.pdf
-// page 17
+/*
+
+  MIT License
+
+  Copyright (c) 2024-2025 Yafiyogi
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+
+*/
 
 #pragma once
 
+#include <type_traits>
+
+#include "yy_types.hpp"
+
 namespace yafiyogi::yy_data {
 
+// Based on https://github.com/CppCon/CppCon2019/blob/master/Presentations/the_smart_pointers_i_wish_i_had/the_smart_pointers_i_wish_i_had__matthew_fleming__cppcon_2019.pdf
+// page 17
+
 template <typename T>
-class observer_ptr
+class observer_ptr final
 {
   public:
-    using value_type = T;
+    using value_type = std::remove_volatile_t<std::remove_reference_t<T>>;
     using pointer = std::add_pointer_t<T>;
+    using const_pointer = std::add_pointer_t<std::add_const_t<T>>;
 
     constexpr explicit observer_ptr(pointer p) noexcept:
       m_raw_ptr(p)
@@ -28,7 +59,7 @@ class observer_ptr
 
     constexpr observer_ptr & operator=(const observer_ptr &) noexcept = default;
     constexpr observer_ptr & operator=(observer_ptr &&) noexcept = default;
-    constexpr observer_ptr & operator=(const pointer other)
+    constexpr observer_ptr & operator=(pointer other)
     {
       m_raw_ptr = other;
 
@@ -71,22 +102,161 @@ class observer_ptr
 
     constexpr explicit operator bool() const noexcept
     {
+      return !empty();
+    }
+
+    constexpr bool empty() const noexcept
+    {
       return nullptr == m_raw_ptr;
     }
 
-    constexpr pointer get() const noexcept
+    constexpr pointer get() noexcept
     {
       return m_raw_ptr;
     }
 
-    constexpr pointer operator->() const noexcept
+    constexpr const_pointer get() const noexcept
     {
       return m_raw_ptr;
     }
 
-    constexpr T & operator*() const noexcept
+    constexpr pointer operator->() noexcept
+    {
+      return m_raw_ptr;
+    }
+
+    constexpr const_pointer operator->() const noexcept
+    {
+      return m_raw_ptr;
+    }
+
+    constexpr T & operator*() noexcept
     {
       return *m_raw_ptr;
+    }
+
+    constexpr const T & operator*() const noexcept
+    {
+      return *m_raw_ptr;
+    }
+
+    constexpr observer_ptr & operator++() noexcept
+    {
+      ++m_raw_ptr;
+
+      return *this;
+    }
+
+    constexpr observer_ptr operator++(int) noexcept
+    {
+      return observer_ptr{m_raw_ptr++};
+    }
+
+    constexpr observer_ptr & operator+=(ssize_type p_offset) noexcept
+    {
+      m_raw_ptr += p_offset;
+
+      return *this;
+    }
+
+    constexpr observer_ptr & operator+=(int p_offset) noexcept
+    {
+      m_raw_ptr += p_offset;
+
+      return *this;
+    }
+
+    constexpr observer_ptr & operator+=(size_type p_offset) noexcept
+    {
+      m_raw_ptr += p_offset;
+
+      return *this;
+    }
+
+    friend constexpr observer_ptr operator+(observer_ptr p_iter,
+                                            ssize_type p_offset) noexcept
+    {
+      p_iter += p_offset;
+
+      return p_iter;
+    }
+
+    friend constexpr observer_ptr operator+(observer_ptr p_iter,
+                                            int p_offset) noexcept
+    {
+      p_iter += p_offset;
+
+      return p_iter;
+    }
+
+    friend constexpr observer_ptr operator+(observer_ptr p_iter,
+                                            size_type p_offset) noexcept
+    {
+      p_iter += p_offset;
+
+      return p_iter;
+    }
+
+    constexpr observer_ptr & operator--() noexcept
+    {
+      --m_raw_ptr;
+
+      return *this;
+    }
+
+    constexpr observer_ptr operator--(int) noexcept
+    {
+      return observer_ptr{m_raw_ptr--};
+    }
+
+    constexpr observer_ptr & operator-=(ssize_type p_offset) noexcept
+    {
+      m_raw_ptr -= p_offset;
+
+      return *this;
+    }
+
+    constexpr observer_ptr & operator-=(int p_offset) noexcept
+    {
+      m_raw_ptr -= p_offset;
+
+      return *this;
+    }
+
+    constexpr observer_ptr & operator-=(size_type p_offset) noexcept
+    {
+      m_raw_ptr -= p_offset;
+
+      return *this;
+    }
+
+    constexpr ssize_type operator-(const observer_ptr & other) const noexcept
+    {
+      return m_raw_ptr - other.m_raw_ptr;
+    }
+
+    friend constexpr observer_ptr operator-(observer_ptr p_iter,
+                                            ssize_type p_offset) noexcept
+    {
+      p_iter -= p_offset;
+
+      return p_iter;
+    }
+
+    friend constexpr observer_ptr operator-(observer_ptr p_iter,
+                                            int p_offset) noexcept
+    {
+      p_iter -= p_offset;
+
+      return p_iter;
+    }
+
+    friend constexpr observer_ptr operator-(observer_ptr p_iter,
+                                            size_type p_offset) noexcept
+    {
+      p_iter -= p_offset;
+
+      return p_iter;
     }
 
     constexpr void swap(observer_ptr & other) noexcept
