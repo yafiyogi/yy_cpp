8d29d7bb0ae78b6b733f6a0b84bb6435a3bc1bda
yafiyogi <1446544+yafiyogi@users.noreply.github.com>
2025-04-13 20:33:55 +0100

    yy_compare_util.h: refactor for flat_map & falt_set.

--- yy_compare_util.h
+++ yy_compare_util.h
@@ -37,42 +37,93 @@ namespace compare_util_detail {
 template<typename A,
          typename B,
          typename Enable = void>
+struct is_string:
+      std::false_type
+{
+};
+
+template<typename A,
+         typename B>
+struct is_string<A, B,
+                 typename std::enable_if_t<(yy_traits::is_std_string_v<A>
+                                            || yy_traits::is_std_string_view_v<A>)
+                                           && (yy_traits::is_std_string_v<B>
+                                               || yy_traits::is_std_string_view_v<B>)>>:
+      std::true_type
+{
+};
+
+template<typename A,
+         typename B>
+inline constexpr bool is_string_v = is_string<A, B>::value;
+
+template<typename A,
+         typename B>
+using is_string_t = typename is_string<A, B>::type;
+
+} // namespace compare_util_detail
+
+template<typename Lhs,
+         typename Rhs,
+         typename Enable = void>
 struct Compare final
 {
-    using a_type = yy_traits::remove_cvr_t<A>;
-    using b_type = yy_traits::remove_cvr_t<B>;
+    using lhs_type = yy_traits::remove_cvr_t<Lhs>;
+    using rhs_type = yy_traits::remove_cvr_t<Rhs>;
 
-    static_assert(std::is_same_v<typename a_type::value_type, typename b_type::value_type>,
+    static_assert(std::is_same_v<typename lhs_type::value_type, typename rhs_type::value_type>,
                   "Must be comparing the same value_type!");
 
-    static constexpr bool equal(const a_type & a, const b_type & b) noexcept
+    static constexpr bool equal(const lhs_type & lhs,
+                                const rhs_type & rhs) noexcept
     {
-      const auto a_size{a.size()};
-      const auto a_begin{a.data()};
+      const auto lhs_size{lhs.size()};
+      const auto lhs_begin{lhs.data()};
 
-      return (a_size == b.size())
-        && std::equal(a_begin, a_begin + a_size, b.data());
+      return (lhs_size == rhs.size())
+        && std::equal(lhs_begin, lhs_begin + lhs_size, rhs.data());
     }
 
-    static constexpr bool less_than(const a_type & a, const b_type & b) noexcept
+    static constexpr bool less_than(const lhs_type & lhs,
+                                    const rhs_type & rhs) noexcept
     {
-      const auto a_size{a.size()};
-      const auto a_begin{a.data()};
-      const auto b_size{b.size()};
-      const auto b_begin{b.data()};
+      const auto lhs_size{lhs.size()};
+      const auto lhs_begin{lhs.data()};
+      const auto rhs_size{rhs.size()};
+      const auto rhs_begin{rhs.data()};
 
-      return std::lexicographical_compare(a_begin, a_begin + a_size,
-                                          b_begin, b_begin + b_size);
+      return std::lexicographical_compare(lhs_begin, lhs_begin + lhs_size,
+                                          rhs_begin, rhs_begin + rhs_size);
+    }
+};
+
+template<typename Lhs,
+         typename Rhs>
+struct Compare<Lhs, Rhs,
+               std::enable_if_t<std::is_arithmetic_v<Lhs>
+                                && std::is_arithmetic_v<Rhs>>> final
+{
+    using lhs_type = yy_traits::remove_cvr_t<Lhs>;
+    using rhs_type = yy_traits::remove_cvr_t<Rhs>;
+
+    static_assert(std::is_same_v<lhs_type, rhs_type>,
+                  "Must be comparing the same value_type!");
+
+    static constexpr bool equal(const lhs_type & a, const rhs_type & b) noexcept
+    {
+      return a == b;
+    }
+
+    static constexpr bool less_than(const lhs_type & a, const rhs_type & b) noexcept
+    {
+      return a < b;
     }
 };
 
 template<typename A,
          typename B>
 struct Compare<A, B,
-               std::enable_if_t<(yy_traits::is_std_string_v<A>
-                                 || yy_traits::is_std_string_view_v<A>)
-                                && (yy_traits::is_std_string_v<B>
-                                    || yy_traits::is_std_string_view_v<B>)>> final
+               std::enable_if_t<compare_util_detail::is_string_v<A, B>>> final
 {
     using a_type = yy_traits::remove_cvr_t<A>;
     using b_type = yy_traits::remove_cvr_t<B>;
@@ -91,8 +142,9 @@ struct Compare<A, B,
 template<typename A,
          typename B>
 struct Compare<A, B,
-               std::enable_if_t<std::is_same_v<typename yy_traits::remove_cvr_t<A>::value_type,
-                                               typename yy_traits::remove_cvr_t<B>::value_type>
+               std::enable_if_t<!compare_util_detail::is_string_v<A, B>
+                                && std::is_same_v<typename yy_traits::remove_cvr_t<A>::value_type,
+                                                  typename yy_traits::remove_cvr_t<B>::value_type>
                                 && (std::is_same_v<char, typename yy_traits::remove_cvr_t<A>::value_type>
                                     || std::is_same_v<char8_t, typename yy_traits::remove_cvr_t<A>::value_type>)>> final
 {
@@ -120,20 +172,18 @@ struct Compare<A, B,
     }
 };
 
-} // namespace compare_util_detail
-
 template<typename A,
          typename B>
 constexpr bool equal(const A & a, const B & b) noexcept
 {
-  return compare_util_detail::Compare<A, B>::equal(a, b);
+  return Compare<A, B>::equal(a, b);
 }
 
 template<typename A,
          typename B>
 constexpr bool less_than(const A & a, const B & b) noexcept
 {
-  return compare_util_detail::Compare<A, B>::less_than(a, b);
+  return Compare<A, B>::less_than(a, b);
 }
 
 
