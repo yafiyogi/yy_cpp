6294c979669f6d955532004de723adba74fa936c
yafiyogi <1446544+yafiyogi@users.noreply.github.com>
2025-05-05 14:29:23 +0100

    yy_trie_node_idx.hpp: remove flat_map<> and use simple_vector<> to hold edges & labels.

--- yy_trie_node_idx.hpp
+++ yy_trie_node_idx.hpp
@@ -27,10 +27,11 @@
 #pragma once
 
 #include "yy_constants.hpp"
-#include "yy_flat_map.h"
+#include "yy_find_raw_util.hpp"
 #include "yy_span.h"
 #include "yy_trie_common.h"
 #include "yy_type_traits.h"
+#include "yy_vector.h"
 
 namespace yafiyogi::yy_data::trie_node_idx_detail {
 
@@ -50,11 +51,12 @@ struct trie_node_idx_traits final
     using label_r_value_ref = typename label_traits::label_r_value_ref;
 
     using value_type = typename value_traits::value_type;
+    using value_ptr = typename value_traits::value_ptr;
 
     using node_type = trie_node_idx<LabelType, ValueType>;
     using node_idx_type = size_type;
     using data_idx_type = size_type;
-    using edges_type = flat_map<label_type, node_idx_type>;
+    using data_idx_ptr = std::add_pointer_t<data_idx_type>;
 };
 
 template<typename LabelType,
@@ -69,11 +71,12 @@ class trie_node_idx final
     using label_r_value_ref = typename traits::label_r_value_ref;
 
     using value_type = typename traits::value_type;
+    using value_ptr = typename traits::value_ptr;
     using data_idx_type = typename traits::data_idx_type;
+    using data_idx_ptr = typename traits::data_idx_ptr;
 
     using node_idx_type = typename traits::node_idx_type;
     using node_type = typename traits::node_type;
-    using edges_type = typename traits::edges_type;
 
     static constexpr node_idx_type root_idx = 0;
     static constexpr node_idx_type empty_idx = end_idx;
@@ -91,35 +94,42 @@ class trie_node_idx final
     constexpr trie_node_idx & operator=(const trie_node_idx & node) noexcept = default;
     constexpr trie_node_idx & operator=(trie_node_idx && node) noexcept = default;
 
-    constexpr void add_edge(size_type pos,
-                            label_r_value_ref label,
-                            node_idx_type node_idx)
+    constexpr void add_edge(data_idx_ptr p_edge,
+                            label_r_value_ref p_label,
+                            node_idx_type p_node_idx)
     {
-      std::ignore = m_edges.emplace(pos, std::move(label), node_idx);
+      auto pos = p_edge - m_edges.data();
+
+      std::ignore = m_labels.emplace(m_labels.begin() + pos, std::move(p_label));
+      std::ignore = m_edges.emplace(m_edges.begin() + pos, p_node_idx);
+    }
+
+    struct found_value_type
+    {
+        data_idx_ptr edge = nullptr;
+        bool found = false;
+    };
+
+    template<typename InputLabelType>
+    [[nodiscard]]
+    constexpr found_value_type find_edge(const InputLabelType & p_label) noexcept
+    {
+      auto [label, found] = find_raw(m_labels, p_label);
+
+      return found_value_type{m_edges.data() + (label - m_labels.data()), found};
     }
 
     using pos_found_type = yy_data::pos_found_type;
 
-    template<typename Visitor,
-             typename InputLabelType>
-    [[nodiscard]]
-    constexpr pos_found_type find_edge(Visitor && visitor,
-                                       const InputLabelType & label) noexcept
-    {
-      return m_edges.find_value(std::forward<Visitor>(visitor), label);
-    }
-
-    template<typename InputLabelType>
-    [[nodiscard]]
-    constexpr pos_found_type find_edge_pos(const InputLabelType & label) const noexcept
-    {
-      return m_edges.find_pos(label);
-    }
-
     template<typename Visitor>
     constexpr void visit(Visitor && visitor) noexcept
     {
-      m_edges.visit(std::forward<Visitor>(visitor));
+      auto edge = m_edges.data();
+      for(auto & label : m_labels)
+      {
+        visitor(label, *edge);
+        ++edge;
+      }
     }
 
     [[nodiscard]]
@@ -146,7 +156,8 @@ class trie_node_idx final
 
   private:
     data_idx_type m_data = no_data;
-    edges_type m_edges{};
+    yy_quad::simple_vector<label_type> m_labels;
+    yy_quad::simple_vector<node_idx_type> m_edges;
 };
 
 } // yafiyogi::yy_data::trie_node_idx_detail
