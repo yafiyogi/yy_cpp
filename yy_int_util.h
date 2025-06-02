1dc4b8d77e157401669d705b686f6ce2efa07bdd
yafiyogi <1446544+yafiyogi@users.noreply.github.com>
2025-06-02 17:26:58 +0100

    yy_int_util.h: add base.

--- yy_int_util.h
+++ yy_int_util.h
@@ -34,27 +34,29 @@
 namespace yafiyogi::yy_util {
 namespace digits_detail {
 
-template<size_type num>
+template<size_type num,
+         size_type base>
 struct DigitsHelper final
 {
-    static constexpr const size_type digits = 1 + DigitsHelper<num / 10>::digits;
+    static constexpr const size_type digits = 1 + DigitsHelper<num / base, base>::digits;
 };
 
-template<>
-struct DigitsHelper<0> final
+template<size_type base>
+struct DigitsHelper<0, base> final
 {
     static constexpr const size_type digits = 0;
 };
 
 } // namespace digits_detail
 
-template<typename I>
+template<typename I,
+         size_type Base = 10>
 struct Digits final
 {
     using int_type = yy_traits::remove_cvr_t<I>;
     static_assert(std::is_integral_v<int_type>, "Type must be integer type!");
 
-    static constexpr const size_type digits = digits_detail::DigitsHelper<std::numeric_limits<int_type>::max()>::digits;
+    static constexpr const size_type digits = digits_detail::DigitsHelper<std::numeric_limits<int_type>::max(), Base>::digits;
 };
 
 } // namespace yafiyogi::yy_util
