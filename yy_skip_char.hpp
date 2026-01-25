5f06eb0c5f8635be2521cc645fb0782d29b93eb8
yafiyogi <1446544+yafiyogi@users.noreply.github.com>
2026-01-25 12:37:52 +0000

    yy_skip_char.hpp: add.

--- yy_skip_char.hpp
+++ yy_skip_char.hpp
@@ -34,10 +34,8 @@ namespace yafiyogi::yy_util {
 
 template<typename SpanType,
          typename span_type = yy_traits::remove_cvr_t<SpanType>,
-         std::enable_if_t<yy_traits::is_span_v<span_type>
-                            || yy_traits::is_std_string_v<span_type>
-                            || yy_traits::is_std_string_view_v<span_type>,
-                          bool> = true>
+         std::enable_if_t<yy_traits::is_span_v<span_type> || yy_traits::is_std_string_v<span_type>
+                            || yy_traits::is_std_string_view_v<span_type>, bool> = true>
 inline span_type skip_not_char(const span_type src,
                                const typename span_type::value_type c)
 {
@@ -58,23 +56,23 @@ inline span_type skip_not_char(const span_type src,
 }
 
 template<typename T>
-inline const T * skip_not_char(const T * src, size_type size, T c)
+inline const T * skip_not_char(const T * src,
+                               size_type size, T c)
 {
   return skip_not_char(yy_quad::const_span<T>(src, size), c).data();
 }
 
 template<typename T>
-inline const T * skip_not_char(const T * begin, const T * end, T c)
+inline const T * skip_not_char(const T * begin,
+                               const T * end, T c)
 {
   return skip_not_char(yy_quad::const_span<T>(begin, end), c).data();
 }
 
 template<typename SpanType,
          typename span_type = yy_traits::remove_cvr_t<SpanType>,
-         std::enable_if_t<yy_traits::is_span_v<span_type>
-                            || yy_traits::is_std_string_v<span_type>
-                            || yy_traits::is_std_string_view_v<span_type>,
-                          bool> = true>
+         std::enable_if_t<yy_traits::is_span_v<span_type> || yy_traits::is_std_string_v<span_type>
+                            || yy_traits::is_std_string_view_v<span_type>, bool> = true>
 inline span_type skip_char(const span_type src,
                            typename span_type::value_type c)
 {
@@ -95,13 +93,17 @@ inline span_type skip_char(const span_type src,
 }
 
 template<typename T>
-inline const T * skip_char(const T * src, size_type size, T c)
+inline const T * skip_char(const T * src,
+                           size_type size,
+                           T c)
 {
   return skip_char(yy_quad::const_span<T>(src, size), c).data();
 }
 
 template<typename T>
-inline const T * skip_char(const T * begin, const T * end, T c)
+inline const T * skip_char(const T * begin,
+                           const T * end,
+                           T c)
 {
   return skip_char(yy_quad::const_span<T>(begin, end), c).data();
 }
